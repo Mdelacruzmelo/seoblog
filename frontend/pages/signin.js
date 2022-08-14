12057368254186ca1d9dd4458cd605ba53875cc8
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2022-08-14 15:10:31 +0200

    Error handler set for duplicated categories

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -6,7 +6,7 @@ import SigninComponent from '../components/auth/SigninComponent';
 const Signin = () => {
     return (
         <Layout>
-            <h2 className="text-center pt-4 pb-4">Signin page</h2>
+            <h2 className="text-center pt-4 pb-4">Signin</h2>
             <div className="row">
                 <div className="col-md-6 offset-md-3">
                     <SigninComponent />
