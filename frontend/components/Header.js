b3866af87dd4a87d4278d94df00dc4c61c4b7040
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2022-08-15 16:56:53 +0200

    Contact page form added

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,89 +1,109 @@
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import nProgress from 'nprogress';
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import { APP_NAME } from '../config';
+import { signout, isAuth } from '../actions/auth';
 import {
+    Collapse,
     Navbar,
+    NavbarToggler,
+    NavbarBrand,
     Nav,
     NavItem,
     NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
 } from 'reactstrap';
-import { APP_NAME } from '../config'
-import { signout, isAuth } from '../actions/auth'
+import '.././node_modules/nprogress/nprogress.css';
+import Search from './blog/Search';
+
+Router.onRouteChangeStart = url => NProgress.start();
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
-    const [authenticated, setAuthenticated] = useState()
-
-    useEffect(() => {
-        setAuthenticated(isAuth())
-    }, [])
-
-    const router = useRouter()
-
-    useEffect(() => {
-        router.events.on('routeChangeStart', nProgress.start)
-        router.events.on('routeChangeComplete', nProgress.done)
-    }, [])
-
-    const styleHidden = {
-        width: '0px',
-        height: '0px',
-        overflow: 'hidden',
-        position: 'absolute',
-        left: '0px',
-        bottom: '0px'
-    }
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
-        <div>
-            <Navbar>
+        <>
+            <Navbar color="light" light expand="md">
                 <Link href="/">
                     <NavLink className="font-weight-bold">{APP_NAME}</NavLink>
                 </Link>
-                <Nav className="me-auto">
-
-                    {!authenticated && (
-                        <>
-                            <NavItem style={{ cursor: 'pointer' }}>
-                                <Link href="/signin"><NavLink>Signin</NavLink></Link>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <React.Fragment>
+                            <NavItem>
+                                <Link href="/blogs">
+                                    <NavLink>Blogs</NavLink>
+                                </Link>
                             </NavItem>
-                            <NavItem style={{ cursor: 'pointer' }}>
-                                <Link href="/signup"><NavLink>Signup</NavLink></Link>
+
+                            <NavItem>
+                                <Link href="/contact">
+                                    <NavLink>Contact</NavLink>
+                                </Link>
                             </NavItem>
-                        </>
-                    )}
+                        </React.Fragment>
 
-                    {authenticated && authenticated.role === 1 && (
+                        {!isAuth() && (
+                            <React.Fragment>
+                                <NavItem>
+                                    <Link href="/signin">
+                                        <NavLink>Signin</NavLink>
+                                    </Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link href="/signup">
+                                        <NavLink>Signup</NavLink>
+                                    </Link>
+                                </NavItem>
+                            </React.Fragment>
+                        )}
+
+                        {isAuth() && isAuth().role === 0 && (
+                            <NavItem>
+                                <Link href="/user">
+                                    <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                                </Link>
+                            </NavItem>
+                        )}
+
+                        {isAuth() && isAuth().role === 1 && (
+                            <NavItem>
+                                <Link href="/admin">
+                                    <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                                </Link>
+                            </NavItem>
+                        )}
+
+                        {isAuth() && (
+                            <NavItem>
+                                <NavLink style={{ cursor: 'pointer' }} onClick={() => signout(() => Router.replace(`/signin`))}>
+                                    Signout
+                                </NavLink>
+                            </NavItem>
+                        )}
+
                         <NavItem>
-                            <Link href="/admin">
-                                <NavLink>{`${authenticated?.name}'s Dashboard`}</NavLink>
-                            </Link>
+                            <a href="/user/crud/blog" className="btn btn-primary text-light">
+                                Write a blog
+                            </a>
                         </NavItem>
-                    )}
-
-                    {authenticated && authenticated.role === 0 && (
-                        <NavItem>
-                            <Link href="/user">
-                                <NavLink>{`${authenticated?.name}'s Dashboard`}</NavLink>
-                            </Link>
-                        </NavItem>
-                    )}
-
-                    {authenticated && (
-                        <NavItem>
-                            <NavLink
-                                style={{ cursor: 'pointer' }}
-                                onClick={() => { signout(() => router.replace(`/signin`)) }}>
-                                Signout
-                            </NavLink>
-                        </NavItem>
-                    )}
-
-                </Nav>
+                    </Nav>
+                </Collapse>
             </Navbar>
-        </div >
+            <Search />
+        </>
     );
-}
+};
 
 export default Header;
