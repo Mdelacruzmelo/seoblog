6f49dbd01650fbda1bb424fc2ce6706316a60de4
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2022-08-15 14:17:15 +0200

    Frontend prepared, new packages. Blog crud done

---
+++ frontend/components/crud/BlogRead.js
@@ -0,0 +1,93 @@
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import Router from 'next/router';
+import { isAuth } from '../../actions/auth';
+import { getCookie } from 'cookies-next';
+import { list, removeBlog } from '../../actions/blog';
+import moment from 'moment';
+
+const BlogRead = ({ username }) => {
+
+    const [token, setToken] = useState()
+    const [blogs, setBlogs] = useState([]);
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        loadBlogs();
+        setToken(getCookie('token'))
+    }, []);
+
+    const loadBlogs = () => {
+        list(username).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setBlogs(data);
+            }
+        });
+    };
+
+    const deleteBlog = slug => {
+        removeBlog(slug, token).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                setMessage(data.message);
+                loadBlogs();
+            }
+        });
+    };
+
+    const deleteConfirm = slug => {
+        let answer = window.confirm('Are you sure you want to delete your blog?');
+        if (answer) {
+            deleteBlog(slug);
+        }
+    };
+
+    const showUpdateButton = blog => {
+        if (isAuth() && isAuth().role === 0) {
+            return (
+                <Link href={`/user/crud/${blog.slug}`}>
+                    <a className="ml-2 btn btn-sm btn-warning">Update</a>
+                </Link>
+            );
+        } else if (isAuth() && isAuth().role === 1) {
+            return (
+                <Link href={`/admin/crud/${blog.slug}`}>
+                    <a className="ml-2 btn btn-sm btn-warning">Update</a>
+                </Link>
+            );
+        }
+    };
+
+    const showAllBlogs = () => {
+        return blogs.map((blog, i) => {
+            return (
+                <div key={i} className="pb-5">
+                    <h3>{blog.title}</h3>
+                    <p className="mark">
+                        Written by {blog.postedBy.name} | Published on {moment(blog.updatedAt).fromNow()}
+                    </p>
+                    <button className="btn btn-sm btn-danger" onClick={() => deleteConfirm(blog.slug)}>
+                        Delete
+                    </button>
+                    {showUpdateButton(blog)}
+                </div>
+            );
+        });
+    };
+
+    return (
+        <React.Fragment>
+            <div className="row">
+                <div className="col-md-12">
+                    {message && <div className="alert alert-warning">{message}</div>}
+                    {showAllBlogs()}
+                </div>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default BlogRead;
--- frontend/pages/admin/index.js
+++ frontend/pages/admin/index.js
@@ -1,30 +1,53 @@
-import React from 'react'
-import Link from 'next/link'
-import Layout from '../../components/Layout'
-import Admin from '../../components/auth/Admin'
+import Layout from '../../components/Layout';
+import Admin from '../../components/auth/Admin';
+import Link from 'next/link';
 
 const AdminIndex = () => {
     return (
         <Layout>
             <Admin>
-                <div className="row">
-                    <div className="col-md-12 pt-5 pb-5">
-                        <h3>Admin Dashboard</h3>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12 pt-5 pb-5">
+                            <h2>Admin Dashboard</h2>
+                        </div>
+                        <div className="col-md-4">
+                            <ul className="list-group">
+                                <li className="list-group-item">
+                                    <Link href="/admin/crud/category-tag">
+                                        <a>Create Category</a>
+                                    </Link>
+                                </li>
+
+                                <li className="list-group-item">
+                                    <Link href="/admin/crud/category-tag">
+                                        <a>Create Tag</a>
+                                    </Link>
+                                </li>
+
+                                <li className="list-group-item">
+                                    <a href="/admin/crud/blog">Create Blog</a>
+                                </li>
+
+                                <li className="list-group-item">
+                                    <Link href="/admin/crud/blogs">
+                                        <a>Update/Delete Blog</a>
+                                    </Link>
+                                </li>
+
+                                <li className="list-group-item">
+                                    <Link href="/user/update">
+                                        <a>Update Profile</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-md-8">right</div>
                     </div>
-                    <div className="col-md-4">
-                        <ul className="list-group">
-                            <li className="list-group-item">
-                                <Link href="/admin/crud/category-tag">
-                                    <a>Create category</a>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="col-md-8">Right</div>
                 </div>
             </Admin>
-        </Layout >
+        </Layout>
     );
-}
+};
 
 export default AdminIndex;
